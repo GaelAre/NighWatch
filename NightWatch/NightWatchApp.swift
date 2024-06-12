42ea70b9b7ca523e88d70595bc0198d1e5880b31
GaelAre <116774311+GaelAre@users.noreply.github.com>
2024-06-11 20:05:23 -0400

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Gael Arellano on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
